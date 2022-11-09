aa77ab0d2ce127eacbd942c59e30dc570de9ce0b
Sara Gunnars <gunnars.sara@gmail.com>
2022-11-09 02:09:07 +0100

    dashboard continue

---
+++ Code-alongs/L5-stocky_dashboard/main1.py
@@ -0,0 +1,14 @@
+import dash
+import dash_bootstrap_components as dbc
+import os
+from load_data import StockData
+
+directory_path = os.path.dirname(__file__)
+path = os.path.join(directory_path, "stocks_data")
+
+print(path)
+
+stockdata_object = StockData(path)
+print(stockdata_object.stock_dataframe("AAPL"))
+
+
